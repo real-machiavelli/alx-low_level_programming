1788788f4b436853d3190a1879fae795960c78f3
real-machialli <chinoprecious29@gmail>
2023-03-02 07:35:29 -0800

    new files

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - Entrypoint
+ *
+ * Return Alway 0 (success)
+ */ 
+int main(void)
+{
+	puts("\"programming is like building a nultilingual puzzle")
+		return(0); 
+}
