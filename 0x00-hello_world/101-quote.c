776bb42d2b32206fb4c6cb0557fce0568c359252
real-machialli <chinoprecious29@gmail>
2023-03-02 21:54:02 -0800

    hello good people of the world

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Aways 1 (success)
+ */
+{
+	write(2,"and that piece of art is useful\" - Dora korpa, 2015-10-19\nn", 59);
+	return(1);
+}
