3709285f4d224111008122b39514097bc1773412
real-machiavelli <chinoprecious29@gmail.com>
2023-05-22 16:18:25 -0700

    progress

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  * main - Entrypoint
  *
- * Return Alway 0 (success)
- */ 
+ * Return Alway 0 success
+ */
 int main(void)
 {
-puts("\"programming is like building a nultilingual puzzle\n");
-return (0); 
+puts("\"programming is like building a nultilingual puzzle");
+return (0);
 }
